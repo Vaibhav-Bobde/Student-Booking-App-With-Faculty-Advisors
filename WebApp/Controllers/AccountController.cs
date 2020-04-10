cbe991a7360138b7cfc2e1d17a9c62bc37e688b7
Vaibhav Bobde <vaibhavbobde@csu.fullerton.edu>
2020-04-09 18:54:31 -0700

    First Commit from Visual Studio

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(WebApp.Models.User user)
+        {
+            if(this.IsValidUser(user.Uname, user.Password))
+            {
+                FormsAuthentication.SetAuthCookie(user.Uname, true);
+                return RedirectToAction("Home");
+            }
+            @ViewBag.LoginStatus = false;
+            return View();
+        }
+        private bool IsValidUser(string uname, string pwd)
+        {
+            return uname == "admin" && pwd == "admin";
+        }
+        [Authorize]
+        public ActionResult Home()
+        {
+            string userName = User.Identity.Name;
+            return View();
+        }
+        [Authorize]
+        public void LogOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
